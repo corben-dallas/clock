9434343cc4818139ee9677b2fd73a83265b92234
Corbens <albert.u1994@gmail.com>
2020-09-18 01:12:02 +0300

    Updates

---
+++ precache-manifest.1ff8abb6d806d61cf892497729888b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eba7788be94bf6c1583c632c9c60e0",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "02a824dfdad47410f95f",
+    "url": "/clock/static/css/2.c8e6cc25.chunk.css"
+  },
+  {
+    "revision": "e81b7ffb0faa21850674",
+    "url": "/clock/static/css/main.f626a3ba.chunk.css"
+  },
+  {
+    "revision": "02a824dfdad47410f95f",
+    "url": "/clock/static/js/2.536d77c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.536d77c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81b7ffb0faa21850674",
+    "url": "/clock/static/js/main.21965557.chunk.js"
+  },
+  {
+    "revision": "9fbb0c64d7d7588fb297",
+    "url": "/clock/static/js/runtime-main.bf40dc10.js"
+  }
+]);
